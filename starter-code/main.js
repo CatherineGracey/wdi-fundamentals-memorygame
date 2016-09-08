2dde7459fca77cca3786144f05619c4219eb7a75
Catherine Gracey <cgracey@hotmail.com>
2016-09-08 16:27:25 +1000

    Section 9 - initial logic to compare card values.

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,6 +1,22 @@
-console.log("JS file is connected to HTML! Woo!")
-
 var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "king";
 var cardFour = "queen";
+
+if (cardTwo === cardFour){
+  alert("You found a match!");
+} else {
+  alert("Sorry, try again.");
+}
+
+if (cardTwo === cardThree){
+  alert("You found a match!");
+} else {
+  alert("Sorry, try again.");
+}
+
+if (cardOne === cardFour){
+  alert("You found a match!");
+} else {
+  alert("Sorry, try again.");
+}
